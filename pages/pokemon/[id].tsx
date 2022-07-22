8c0a82ea90c511db1fcd52d9ba6ab845b6302c8d
AlexisToribio <alexistoba21@hotmail.com>
2022-07-22 16:27:51 -0500

    feat: add ISR and ISG

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -115,15 +115,26 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
   const pokemons151 = [...Array(151)].map((valor, index) => `${index + 1}`);
   return {
     paths: pokemons151.map((id) => ({ params: { id } })),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   return {
-    props: { pokemon: await getPokemonInfo(id) },
+    props: { pokemon },
+    revalidate: 86400, // 60 * 60 * 24,
   };
 };
 
