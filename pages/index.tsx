4f672b1ddf8a391dd74ef433cc478e4742b9493c
AlexisToribio <alexistoba21@hotmail.com>
2022-07-22 15:28:22 -0500

    feat: add searcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,44 @@
+import { ChangeEvent, useState } from 'react';
+
 import { NextPage, GetStaticProps } from 'next';
 
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts';
 import { PokemonCard } from '../components/pokemon';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
-import { Grid } from '@nextui-org/react';
+import { FormElement, Grid, Input } from '@nextui-org/react';
 
 interface Props {
   pokemons: SmallPokemon[];
 }
 
 const HomePage: NextPage<Props> = ({ pokemons }) => {
+  const [searchPokemon, setSearchPokemon] = useState<string>('');
+  const handleChange = (e: ChangeEvent<FormElement>) => {
+    setSearchPokemon(e.currentTarget.value);
+  };
+
+  const results = !searchPokemon
+    ? pokemons
+    : pokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchPokemon.toLowerCase())
+      );
+
   return (
     <Layout title="Listado de Pokemons">
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Input
+          labelPlaceholder="Busca un pokemon"
+          value={searchPokemon}
+          width="500px"
+          placeholder="Busca un pokemon"
+          css={{ marginTop: '45px', marginBottom: '18px' }}
+          onChange={handleChange}
+        />
+      </div>
+
       <Grid.Container gap={2} justify="flex-start">
-        {pokemons.map((pokemon) => (
+        {results.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}
       </Grid.Container>
